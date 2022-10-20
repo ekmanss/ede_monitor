05b878862c6e848fbceb3e10b36f9ba3b2713ece
Roy <hfers@msn.com>
2022-10-20 18:42:59 +0800

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import React, {lazy, useEffect, useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {ethers} from 'ethers'
 
 import useIdo from "./hooks/useIdo";
 import {ede_bot_executor} from "./config/address"
