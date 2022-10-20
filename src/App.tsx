c62ac7ce233aa732fcd0ede233b39aa78a671c5f
Roy <hfers@msn.com>
2022-10-20 18:41:47 +0800

    done

--- src/App.tsx
+++ src/App.tsx
@@ -4,37 +4,29 @@ import './App.css';
 import {ethers} from 'ethers'
 
 import useIdo from "./hooks/useIdo";
-
+import {ede_bot_executor} from "./config/address"
 
 function App() {
 
     const {
-        marketOrderExecutor,
-        priceFeedExecutor,
-        limitOrderExecutor,
-        limitDecreaseOrderExecutor,
-        limitSwapOrderExecutor,
-        liquidationExecutor
-    } = useIdo()
+        all
+    } = useIdo(ede_bot_executor)
 
-    marketOrderExecutor.indexOf('.')
 
     return (
         <div className="App">
             <header className="App-header">
                 <div>
-                    marketOrderExecutor:0xD5F8DaE587d0d361caC4B05741Be33a65169c38E
-                    = {Number(marketOrderExecutor).toFixed(3)}<br/>
-                    priceFeedExecutor:0x9fC9bd6a1605e78bA4556f0098442aB93E78FC0D
-                    = {Number(priceFeedExecutor).toFixed(3)}<br/>
-                    limitOrderExecutor:0xBd5866946Cd188F3dcBa5E5c59855cD4768F7D4A
-                    = {Number(limitOrderExecutor).toFixed(3)}<br/>
-                    limitDecreaseOrderExecutor:0x05cc2300Bc96D4DFE1bC95177d58304e8EB25a6f
-                    = {Number(limitDecreaseOrderExecutor).toFixed(3)}<br/>
-                    limitSwapOrderExecutor:0x71722Ff6E91B27F7AA336FDbDe7B6B31016Dd9f4
-                    = {Number(limitSwapOrderExecutor).toFixed(3)}<br/>
-                    liquidationExecutor:0x7118D92279fFFf53f281dd6D315cA8BFa29714aE
-                    = {Number(liquidationExecutor).toFixed(3)}<br/>
+                    {all.map((item, index) => {
+                        return (
+                            <div key={index}>
+                                <div>{item.name}</div>
+                                <div>{item.address}</div>
+                                <div>{item.balance}</div>
+                                <br/>
+                            </div>
+                        )
+                    })}
                 </div>
             </header>
         </div>
--- src/hooks/useIdo.ts
+++ src/hooks/useIdo.ts
@@ -3,7 +3,15 @@ import {useState, useEffect, useMemo, useCallback, useRef} from 'react'
 
 const provider = new ethers.providers.JsonRpcProvider("https://data-seed-prebsc-1-s2.binance.org:8545/");
 
-export default function useIdo() {
+interface BalanceItme {
+    balance: string,
+    name: string,
+    address: string,
+    chainId: number,
+}
+
+
+export default function useIdo(ede_bot_executor: BalanceItme[]) {
 
     const [value, setValue] = useState<number>(0);
     const [timers, setTimers] = useState<Array<NodeJS.Timeout>>([]);
@@ -21,21 +29,16 @@ export default function useIdo() {
         const tick = () => {
             saveCallBack.current();
         };
-        const timer: NodeJS.Timeout = setInterval(tick, 10000);
+        const timer: NodeJS.Timeout = setInterval(tick, 1000 * 10);
         timers.push(timer);
         setTimers(timers);
-        console.log(timers);
+        console.log("timer::", timers);
         return () => {
             clearInterval(timer);
         };
     }, []);
     //=========================================
-    const [marketOrderExecutor, setMarketOrderExecutor] = useState('0')
-    const [priceFeedExecutor, setPriceFeedExecutor] = useState('0')
-    const [limitOrderExecutor, setLimitOrderExecutor] = useState('0')
-    const [limitDecreaseOrderExecutor, setLimitDecreaseOrderExecutor] = useState('0')
-    const [limitSwapOrderExecutor, setLimitSwapOrderExecutor] = useState('0')
-    const [liquidationExecutor, setLiquidationExecutor] = useState('0')
+    const [all, setAll] = useState([] as BalanceItme[])
 
     useEffect(() => {
         initData().then()
@@ -48,23 +51,16 @@ export default function useIdo() {
             try {
                 const list = ["0xD5F8DaE587d0d361caC4B05741Be33a65169c38E", "0x9fC9bd6a1605e78bA4556f0098442aB93E78FC0D", "0xBd5866946Cd188F3dcBa5E5c59855cD4768F7D4A", "0x05cc2300Bc96D4DFE1bC95177d58304e8EB25a6f", "0x71722Ff6E91B27F7AA336FDbDe7B6B31016Dd9f4", "0x7118D92279fFFf53f281dd6D315cA8BFa29714aE"]
 
-                for (var i = 0; i < list.length; i++) {
-                    let rs = await provider.getBalance(list[i]);
+                let allBalance: BalanceItme[] = [];
+                for (var i = 0; i < ede_bot_executor.length; i++) {
+                    let rs = await provider.getBalance(ede_bot_executor[i].address);
                     let bal = ethers.utils.formatEther(rs);
-                    console.log(" balance ................ = " + bal);
-                    if (i === 0)
-                        setMarketOrderExecutor(bal);
-                    if (i === 1)
-                        setPriceFeedExecutor(bal);
-                    if (i === 2)
-                        setLimitOrderExecutor(bal);
-                    if (i === 3)
-                        setLimitDecreaseOrderExecutor(bal);
-                    if (i === 4)
-                        setLimitSwapOrderExecutor(bal);
-                    if (i === 5)
-                        setLiquidationExecutor(bal);
+                    ede_bot_executor[i].balance = Number(bal).toFixed(3);
+                    allBalance.push(ede_bot_executor[i])
                 }
+                console.log(Date.now())
+                setAll(allBalance);
+
             } catch (error) {
                 console.log(error)
             }
@@ -73,12 +69,7 @@ export default function useIdo() {
         [value])
 
     return {
-        marketOrderExecutor,
-        priceFeedExecutor,
-        limitOrderExecutor,
-        limitDecreaseOrderExecutor,
-        limitSwapOrderExecutor,
-        liquidationExecutor
+        all
     }
 }
 
